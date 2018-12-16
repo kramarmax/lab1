45f65a92fc642d0b69b943db51c0b4c8dfce15ea
kramarmax <44787412+kramarmax@users.noreply.github.com>
2018-12-16 22:45:53 +0300

    Update Exception.hpp

--- Exception.hpp
+++ Exception.hpp
@@ -1,11 +1,12 @@
 #pragma once
 #include <string>
 
-class Exception{
-	std::string message;//сообщение исклюени€
+class Exception
+{
+	std::string message;//сообщение исклюение
 public:
 	Exception(const std::string& message = "Exception");
 	Exception(const Exception& e);
 	~Exception();
-	const std::string& what()const;//выводим сообщение исключени€
+	const std::string& what()const;//выводим сообщение исключение
 };
